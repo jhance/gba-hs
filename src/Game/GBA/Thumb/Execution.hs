d007d3b000f46501b6a24cfd620e0efbd8b9bc90
Jared Hance <jared.hance@case.edu>
2014-11-15 16:43:10 -0500

    Add/subtract

--- src/Game/GBA/Thumb/Execution.hs
+++ src/Game/GBA/Thumb/Execution.hs
@@ -2,6 +2,7 @@ module Game.GBA.Thumb.Execution
     (execute)
 where
 
+import           Control.Applicative
 import           Data.Bits
 import           Data.Word
 import           Game.GBA.CPUFlag
@@ -38,35 +39,53 @@ executeT1 TSRO_LSR n src dest = do
     setCondition CFCarry $ testBit val (fromIntegral n - 1)
     setZero val'
     setSign val'
+executeT1 TSRO_ASR n src dest = do
+    let n' = fromIntegral n
+    val <- readRegister src
+    let val' = shiftR val n'
+        val'' = if testBit val 31
+                    then shiftL (complement 0) (32 - n') + val'
+                    else val'
+    writeRegister dest val''
+    setCondition CFCarry $ testBit val (n' - 1)
+    setZero val''
+    setSign val''
 
 -- | Gets either the register or the direct number
 -- for a TAS.
-tasValue :: TASOperandType -> Word8 -> GBA s Word32
-tasValue TASO_REG = readRegister . fromIntegral
-tasValue TASO_NUM = return . fromIntegral
 
 tasOperation :: TASOperation -> Word32 -> Word32 -> Word32
 tasOperation TASO_ADD = (+)
 tasOperation TASO_SUB = (-)
 
--- carry, zero
-tasFlags :: TASOperation -> Integer -> Integer -> [(ConditionFlag, Bool)]
-tasFlags = undefined
-
 -- t2
 -----
-executeT2 = undefined
+executeT2 :: TASOperandType -> TASOperation -> Word8
+          -> RegisterID -> RegisterID -> GBA s ()
+executeT2 otype atype operand src dest = do
+    operand' <- tasConvert atype <$> tasValue otype operand
+    val <- readRegister src
+    let sameSign = testBit val 31 == testBit operand' 31
+        result = val + operand'
+    writeRegister dest $ val + operand'
+    setZero result
+    setSign result
+    setCondition CFCarry $ result < val
+    setCondition CFOverflow $ result < val && sameSign
 
--- | This is a big function! And really important!!
+tasValue :: TASOperandType -> Word8 -> GBA s Word32
+tasValue TASO_REG = readRegister . fromIntegral
+tasValue TASO_NUM = return . fromIntegral
+
+tasConvert :: TASOperation -> Word32 -> Word32
+tasConvert TASO_ADD x = x
+tasConvert TASO_SUB x = complement x + 1
+
+-- | Execution of any Thumb mode instruction.
+-- Please do not try to execute outside of Thumb mode.
+-- There is no check.
 execute :: TInstruction -> GBA s ()
-
--- t1
-execute (TSR opcode offset src dest) = executeT1 opcode offset src dest
-
--- t2
-execute (TAS srcType opType operand source dest) = do
-    operand' <- tasValue srcType operand
-    value <- readRegister source
-    let result = tasOperation opType value operand'
-    writeRegister dest result
-    -- TODO CPU FLAGS
+execute (TSR opcode offset src dest) =
+    executeT1 opcode offset src dest
+execute (TAS srcType opType operand source dest) =
+    executeT2 srcType opType operand source dest
--- test/Test/GBA/Thumb/Execution.hs
+++ test/Test/GBA/Thumb/Execution.hs
@@ -8,6 +8,7 @@ import           Control.Applicative
 import           Control.Monad.ST
 import           Language.Literals.Binary
 
+import           Data.Bits
 import           Data.Word
 
 import           Game.GBA.Boot (bootForTest)
@@ -46,7 +47,24 @@ tests = testGroup "execution"
           , shiftRegLSR4
           , shiftRegLSR5
           ]
-      , testGroup "asr (t1.3)" []
+      , testGroup "asr (t1.3)" $
+          [ shiftRegASR1
+          , shiftRegASR2
+          , shiftRegASR3
+          , shiftRegASR4
+          ]
+      ]
+    , testGroup "add/subtract (t2)" $
+      [ testGroup "add (t2.1)" $
+          [ tasAdd1
+          , tasAdd2
+          , tasAdd3
+          , tasAdd4
+          ]
+      , testGroup "subtract (t2.2)" $
+          [tasSub1
+          --, tasSub2
+          ]
       ]
     ]
 
@@ -153,7 +171,7 @@ shiftRegLSL8 = testCase "shift register LSL by 8" $ do
 -------
 -- invariant: n will always be false.
 shiftRegLSR1 :: TestTree
-shiftRegLSR1 = testCase "shift register RSL by 1" $ do
+shiftRegLSR1 = testCase "shift register LSR by 1" $ do
     (dest, c, z, n) <- runTest $ do
         writeRegister [b|010|] $ 2^20
         execute (TSR TSRO_LSR 1 [b|010|] [b|001|])
@@ -164,7 +182,7 @@ shiftRegLSR1 = testCase "shift register RSL by 1" $ do
     n @?= False
 
 shiftRegLSR2 :: TestTree
-shiftRegLSR2 = testCase "shift register RSL by 8" $ do
+shiftRegLSR2 = testCase "shift register LSR by 8" $ do
     (dest, c, z, n) <- runTest $ do
         writeRegister [b|010|] $ 2^20
         execute (TSR TSRO_LSR 8 [b|010|] [b|001|])
@@ -175,7 +193,7 @@ shiftRegLSR2 = testCase "shift register RSL by 8" $ do
     n @?= False
 
 shiftRegLSR3 :: TestTree
-shiftRegLSR3 = testCase "shift register RSL with carry" $ do
+shiftRegLSR3 = testCase "shift register LSR with carry" $ do
     (dest, c, z, n) <- runTest $ do
         writeRegister [b|000|] $ 2^20 + 2
         execute (TSR TSRO_LSR 2 [b|000|] [b|001|])
@@ -186,7 +204,7 @@ shiftRegLSR3 = testCase "shift register RSL with carry" $ do
     n @?= False
 
 shiftRegLSR4 :: TestTree
-shiftRegLSR4 = testCase "shift register RSL to zero" $ do
+shiftRegLSR4 = testCase "shift register LSR to zero" $ do
     (dest, c, z, n) <- runTest $ do
         writeRegister [b|000|] $ 2^20
         execute (TSR TSRO_LSR 30 [b|000|] [b|001|])
@@ -197,7 +215,7 @@ shiftRegLSR4 = testCase "shift register RSL to zero" $ do
     n @?= False
 
 shiftRegLSR5 :: TestTree
-shiftRegLSR5 = testCase "shift register RSL to zero with carry" $ do
+shiftRegLSR5 = testCase "shift register LSR to zero with carry" $ do
     (dest, c, z, n) <- runTest $ do
         writeRegister [b|000|] $ 2^20
         execute (TSR TSRO_LSR 21 [b|000|] [b|001|])
@@ -206,3 +224,124 @@ shiftRegLSR5 = testCase "shift register RSL to zero with carry" $ do
     c @?= True
     z @?= True
     n @?= False
+
+-- t1.3
+-------
+shiftRegASR1 :: TestTree
+shiftRegASR1 = testCase "shift register ASR by 1, no sign bit" $ do
+    (dest, c, z, n) <- runTest $ do
+        writeRegister [b|000|] $ 2^20
+        execute (TSR TSRO_ASR 1 [b|000|] [b|001|])
+        getShiftResult [b|001|]
+    dest @?= 2^19
+    c @?= False
+    z @?= False
+    n @?= False
+
+shiftRegASR2 :: TestTree
+shiftRegASR2 = testCase "shift register ASR by 1, with sign bit" $ do
+    (dest, c, z, n) <- runTest $ do
+        writeRegister [b|000|] $ 2^20 + 2^31
+        execute (TSR TSRO_ASR 1 [b|000|] [b|001|])
+        getShiftResult [b|001|]
+    dest @?= 2^19 + 2^31 + 2^30
+    c @?= False
+    z @?= False
+    n @?= True
+
+shiftRegASR3 :: TestTree
+shiftRegASR3 = testCase "shift register ASR to zero with carry" $ do
+    (dest, c, z, n) <- runTest $ do
+        writeRegister [b|000|] $ 2^20
+        execute (TSR TSRO_ASR 21 [b|000|] [b|001|])
+        getShiftResult [b|001|]
+    dest @?= 0
+    c @?= True
+    z @?= True
+    n @?= False
+
+shiftRegASR4 :: TestTree
+shiftRegASR4 = testCase "shift register ASR with sign bit multiple, with carry" $ do
+    (dest, c, z, n) <- runTest $ do
+        writeRegister [b|000|] $ 2^20 + 2^31 + 2^4
+        execute (TSR TSRO_ASR 5 [b|000|] [b|001|])
+        getShiftResult [b|001|]
+    dest @?= 2^15 + 2^31 + 2^30 + 2^29 + 2^28 + 2^27 + 2^26
+    c @?= True
+    z @?= False
+    n @?= True
+
+-- t3
+-----
+
+getTASResult :: RegisterID -> GBA s (Word32, Bool, Bool, Bool, Bool)
+getTASResult reg = (,,,,)
+    <$> readRegister reg
+    <*> getCondition CFCarry
+    <*> getCondition CFZero
+    <*> getCondition CFSign
+    <*> getCondition CFOverflow
+
+tasAdd1 :: TestTree
+tasAdd1 = testCase "tas add simple immediate" $ do
+    (dest, c, z, n, v) <- runTest $ do
+        writeRegister [b|000|] $ 5
+        execute (TAS TASO_NUM TASO_ADD 4 [b|000|] [b|001|])
+        getTASResult [b|001|]
+    dest @?= 9
+    c @?= False
+    z @?= False
+    n @?= False
+    v @?= False
+
+tasAdd2 :: TestTree
+tasAdd2 = testCase "tas add simple register" $ do
+    (dest, c, z, n, v) <- runTest $ do
+        writeRegister [b|000|] $ 5
+        writeRegister [b|010|] $ 4
+        execute (TAS TASO_REG TASO_ADD [b|010|] [b|000|] [b|001|])
+        getTASResult [b|001|]
+    dest @?= 9
+    c @?= False
+    z @?= False
+    n @?= False
+    v @?= False
+
+tasAdd3 :: TestTree
+tasAdd3 = testCase "tas add simple register, overflow/zero" $ do
+    (dest, c, z, n, v) <- runTest $ do
+        writeRegister [b|000|] $ 2^31
+        writeRegister [b|010|] $ 2^31
+        execute (TAS TASO_REG TASO_ADD [b|010|] [b|000|] [b|001|])
+        getTASResult [b|001|]
+    dest @?= 0
+    c @?= True
+    z @?= True
+    n @?= False
+    v @?= True
+
+tasAdd4 :: TestTree
+tasAdd4 = testCase "tas add simple register, nonsigned overflow" $ do
+    (dest, c, z, n, v) <- runTest $ do
+        writeRegister [b|000|] $ complement 0
+        writeRegister [b|010|] $ complement 0 - 2^31
+        execute (TAS TASO_REG TASO_ADD [b|010|] [b|000|] [b|001|])
+        getTASResult [b|001|]
+    --dest @?= 0
+    c @?= True
+    z @?= False
+    n @?= False
+    v @?= False
+
+tasSub1 :: TestTree
+tasSub1 = testCase "tas sub simple register" $ do
+    (dest, c, z, n, v) <- runTest $ do
+        writeRegister [b|000|] $ 5
+        writeRegister [b|010|] $ 3
+        execute (TAS TASO_REG TASO_SUB [b|010|] [b|000|] [b|001|])
+        getTASResult [b|001|]
+    dest @?= 2
+    c @?= True -- Is this right?!
+    z @?= False
+    n @?= False
+    v @?= False
