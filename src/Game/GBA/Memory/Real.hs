a48ff59d90eab83bb830fb43f07be73d897b71b1
Jared Hance <jared.hance@case.edu>
2014-11-16 14:35:17 -0500

    Fix some breaking stuff with bus width

--- src/Game/GBA/Memory/Real.hs
+++ src/Game/GBA/Memory/Real.hs
@@ -12,6 +12,8 @@ module Game.GBA.Memory.Real
     )
 where
 
+import           Debug.Trace
+
 import           Control.Applicative
 import           Control.Lens
 import           Control.Monad.Base
@@ -31,16 +33,16 @@ writeReal8 addr val = do
 -- Uses little-endian.
 writeReal16 :: RealAddress -> Word16 -> GBA s ()
 writeReal16 (seg, off) val = do
-    writeReal8 (seg, off) . fromIntegral $ shiftL val 8
-    writeReal8 (seg, off+1) . fromIntegral $ shiftR val 8
+    writeReal8 (seg, off) . fromIntegral $ shiftR val 8
+    writeReal8 (seg, off+1) . fromIntegral $ shiftR (shiftL val 8) 8
 
 -- | Write to a real address. (in 32-bit mode)
 --
 -- Uses little-endian.
 writeReal32 :: RealAddress -> Word32 -> GBA s ()
 writeReal32 (seg, off) val = do
-    writeReal16 (seg, off) . fromIntegral $ shiftL val 8
-    writeReal16 (seg, off+2) . fromIntegral $ shiftR val 8
+    writeReal16 (seg, off) . fromIntegral $ shiftR val 16
+    writeReal16 (seg, off+2) . fromIntegral $ shiftR (shiftL val 16) 16
 
 -- | Read from a real address. (in 8-bit mode)
 readReal8 :: RealAddress -> GBA s Word8
--- test/Test/GBA/Memory.hs
+++ test/Test/GBA/Memory.hs
@@ -3,7 +3,10 @@ module Test.GBA.Memory
     (tests)
 where
 
+import           Control.Applicative
 import           Control.Monad.ST
+import           Data.Bits
+import           Data.Word
 
 import           Game.GBA.Boot
 import           Game.GBA.CPUFlag
@@ -24,6 +27,10 @@ tests = testGroup "memory" $
         [ basic1
         , basic2
         ]
+    , testGroup "bus variance" $
+        [ bus1
+        , bus2
+        ]
     , testGroup "timer registers" $
         [
         ]
@@ -42,3 +49,19 @@ basic2 = testCase "write to on-chip WRAM (8-bit)" $ do
         writeVirtual8 0x020000FF 5
         readVirtual8 0x020000FF
     contents @?= 5
+
+bus1 :: TestTree
+bus1 = testCase "write with 8-bit, read with 16-bit" $ do
+    contents <- runTest $ do
+        writeVirtual8 0x02000000 5
+        writeVirtual8 0x02000001 18
+        readVirtual16 0x02000000
+    contents @?= 5 `xor` shiftL 18 8
+
+bus2 :: TestTree
+bus2 = testCase "write with 16-bit, read with 8-bit" $ do
+    (ls, ms) <- runTest $ do
+        writeVirtual16 0x02000000 $ 5 `xor` shiftL 18 8
+        (,) <$> readVirtual8 0x02000000 <*> readVirtual8 0x02000001
+    ls @?= 18
+    ms @?= 5
